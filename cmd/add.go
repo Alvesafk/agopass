e8c0acdb4fa76ddbcbd9b8d49e157ffcfe993096
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-28 09:04:08 -0300

    'add' function working properly, next step is to do the other functions than add the cryptography

--- cmd/add.go
+++ cmd/add.go
@@ -6,13 +6,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/Alvesafk/gopass/format"
 )
-
-type SecretFile struct {
-	Secrets []Secret
-}
 
 type Secret struct {
 	Name string `json:"name"`
@@ -28,27 +25,59 @@ func Add() {
 	fmt.Print(format.Green("Add a Secret:", "bold", 1))
 
 	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Print(format.Yellow("Name of the secret: ", "none", 0))
-
 	name, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-
-	fmt.Print(format.Yellow("Secret word: ", "none", 0))
-
-	secret, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-	}
-
-	new_secret := Secret{name, secret}
-
-	bytes, err := json.Marshal(new_secret)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(bytes))
-}
+	fmt.Print(format.Yellow("Secret word: ", "none", 0))
+	secret, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	new_secret := Secret{strings.TrimSpace(name), strings.TrimSpace(secret)}
+
+	secretsFileStats, err := os.Stat(secrets_path)
+	if err != nil {
+		fmt.Print(format.Red("Secrets file does not exist, use <gopass init>, exiting.", "bold", 1))
+		return
+	}
+
+	if secretsFileStats.Size() <= 10 {
+		all_secrets := []Secret{new_secret}
+		data, err := json.Marshal(all_secrets)
+		if err != nil {
+			log.Fatal(format.Red("Could not serialize secret into JSON", "bold", 1))
+		}
+
+		err = os.WriteFile(secrets_path, data, 0644)
+		if err != nil {
+			log.Fatal(format.Red("Could not write into file: %s", "bold", 1), err)	
+		}
+	} else {
+		var all_secrets []Secret
+
+		data, err := os.ReadFile(secrets_path)
+		if err != nil {
+			log.Fatal(format.Red("Could not read the secrets file.", "bold", 1))
+		}
+
+		json.Unmarshal(data, &all_secrets)
+		all_secrets = append(all_secrets, new_secret)
+
+		writeData, err := json.Marshal(all_secrets)
+		if err != nil {
+			log.Fatal(format.Red("Could not serialize secret into JSON", "bold", 1))
+		}
+
+		err = os.WriteFile(secrets_path, writeData, 0644)
+		if err != nil {
+			log.Fatal(format.Red("Could not write into file: %s", "bold", 1), err)	
+		}
+	}
+
+	fmt.Print(format.Green("Secret was saved!", "bold", 1))
+}
