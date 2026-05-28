023db9e6ca2e35b17a98e8806db77164ab45d7dd
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-28 09:23:03 -0300

    list function, lists all secrets, but don't show the actual passwords

--- cmd/list.go
+++ cmd/list.go
@@ -1,9 +1,41 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/Alvesafk/gopass/format"
 )
 
 func List() {
-	fmt.Println("Hello, from List function.")
+	if !fileExists(config_path) {
+		fmt.Print(format.Red("Secrets file does not exist, use <gopass init>, exiting.", "bold", 1))
+		return
+	}
+
+	var all_secrets []Secret
+
+	data, err := os.ReadFile(secrets_path)
+	if err != nil {
+		log.Fatal(format.Red("Could not read the secrets file.", "bold", 1))
+	}
+
+	json.Unmarshal(data, &all_secrets)
+	
+	for i, v := range all_secrets {
+		name := format.Green(v.Name, "bold", 0)
+
+		fmt.Println(format.Cyan("-------------------------------------------------------------------------------", "none", 0))
+		fmt.Printf("%v - %s\n    %s\n", i + 1, name, hidePassword(v.Key))
+	}
+}
+
+func hidePassword(pass string) string {
+	var result string
+	for i := 0; i < len(pass); i++ {
+		result += "*"
+	}
+	return result
 }
