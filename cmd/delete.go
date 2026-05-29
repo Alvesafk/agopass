b7cc051386f0242086906eaca881b2474084b22e
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-29 08:34:01 -0300

    delete function working, all base functions are now working as intended, going to work now on cryptography

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"math"
+	"os"
+	"strings"
 
 	"github.com/Alvesafk/gopass/color"
 )
@@ -25,6 +29,44 @@ func Delete(args []string) {
 			PrintUsage(args)
 			return
 		}
+	}
+
+	to_get_name := strings.ToLower(strings.TrimSpace(args[2]))
+
+	var all_secrets []Secret
+
+	data, err := os.ReadFile(secrets_path)
+	if err != nil {
+		fmt.Print(color.Red("Could not read the secrets file.", "bold", 1))
+	}
+
+	json.Unmarshal(data, &all_secrets)
+
+	index := math.MinInt
+
+	for i, v := range all_secrets {
+		if strings.ToLower(v.Name) == to_get_name {
+			index = i
+		}
+	}
+
+	if index == math.MinInt {
+		fmt.Print(color.Red("Could not find this key, check your typing.", "bold", 1))
+		return 
+	}
+
+	all_secrets = RemoveSecret(all_secrets, index)
+
+	writeData, err := json.Marshal(all_secrets)
+	if err != nil {
+		fmt.Print(color.Red("Could not serialize secret into JSON", "bold", 1))
+		return
+	}
+
+	err = os.WriteFile(secrets_path, writeData, 0644)
+	if err != nil {
+		fmt.Print(color.Red("Could not write into file: %s", "bold", 1), err)	
+		return
 	}
 }
 
